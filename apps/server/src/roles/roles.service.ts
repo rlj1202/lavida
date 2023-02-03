251982cca8f6d4c3e9ddfc2a7ad1bbe9b2fd7b6a
Jisu Sim <rlj1202@gmail.com>
2023-02-03 15:10:01 +0900

    Server: Add default roles initializing code

--- apps/server/src/roles/roles.service.ts
+++ apps/server/src/roles/roles.service.ts
@@ -1,17 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { TreeRepository } from 'typeorm';
+import { IsNull, TreeRepository } from 'typeorm';
 
 import { Role } from './entities/role.entity';
 
 import { CreatePermissionDto } from './dto/create-permission.dto';
 
+const LOG_CONTEXT = 'RolesService';
+
 @Injectable()
-export class RolesService {
+export class RolesService implements OnModuleInit {
   constructor(
     @InjectRepository(Role)
     private readonly rolesRepository: TreeRepository<Role>,
   ) {}
+
+  async onModuleInit() {
+    Logger.log('Initializing default roles', LOG_CONTEXT);
+
+    const adminRole = await this.rolesRepository.findOne({
+      where: {
+        name: 'admin',
+        parent: IsNull(),
+      },
+    });
+
+    if (!adminRole) {
+      Logger.log('Admin role does not exist, creates one', LOG_CONTEXT);
+
+      const role = new Role();
+      role.name = 'admin';
+      role.permissions = [
+        {
+          action: 'manage',
+          subject: 'all',
+        },
+      ];
+
+      await this.rolesRepository.save(role);
+
+      Logger.log('Admin role has been created', LOG_CONTEXT);
+    } else {
+      Logger.log('Admin role exists', LOG_CONTEXT);
+    }
+  }
 
   async findAll(): Promise<Role[]> {
     const roles = await this.rolesRepository.findTrees();
