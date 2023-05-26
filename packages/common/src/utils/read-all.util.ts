cde006dd67db8d4ae4a9f1372d3fa8fa47fe70d4
Jisu Sim <rlj1202@gmail.com>
2023-05-26 20:10:32 +0900

    Fix(readAll): Listen on finish event

--- packages/common/src/utils/read-all.util.ts
+++ packages/common/src/utils/read-all.util.ts
@@ -7,5 +7,8 @@ export async function readAll(stream: Stream): Promise<Buffer> {
     stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
     stream.on('error', (error) => reject(error));
     stream.on('close', () => resolve(Buffer.concat(chunks)));
+    stream.on('finish', () => {
+      resolve(Buffer.concat(chunks));
+    });
   });
 }
